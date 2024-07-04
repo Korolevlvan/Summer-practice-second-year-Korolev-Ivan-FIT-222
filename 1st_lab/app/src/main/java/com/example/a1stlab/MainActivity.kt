fd1133a66682bff2cad992fcde25710b41a118b6
Azazel <93066733+Korolevlvan@users.noreply.github.com>
2024-07-04 22:23:25 +0600

    layout
    layout, Structure objects, create cards

--- 1st_lab/app/src/main/java/com/example/a1stlab/MainActivity.kt
+++ 1st_lab/app/src/main/java/com/example/a1stlab/MainActivity.kt
@@ -1,47 +1,103 @@
 package com.example.a1stlab
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.a1stlab.ui.theme._1stLabTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _1stLabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Marvel()
         }
     }
 }
 
+
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Marvel() {
+    val listState: LazyListState = rememberLazyListState()
+    LazyColumn(modifier = Modifier
+            .fillMaxSize() ,
+            horizontalAlignment = Alignment.CenterHorizontally
+            , verticalArrangement = Arrangement.SpaceBetween,
+            ) {
+        item(){
+            Image(painter = painterResource(id = R.drawable.img),
+                contentDescription = "Marvel"
+                , modifier = Modifier
+                    .fillParentMaxHeight(0.2f)
+                    .size(300.dp)
+                , contentScale = ContentScale.FillWidth)
+        }
+        item()
+        {
+            Text(text = "Choose Your Hero", fontSize = 15.sp)
+        }
+        item (){
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .fillParentMaxHeight(0.7f)
+                ,horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                state = listState,
+                flingBehavior = rememberSnapFlingBehavior(listState)
+            ) {
+                items(3)
+                {
+                    Card(modifier = Modifier.size(370.dp, 582.dp))
+                    {
+                        Text(text = "Item$it",
+                            modifier = Modifier.offset(162.dp, 291.dp))
+                    }
+                }
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    _1stLabTheme {
-        Greeting("Android")
-    }
+    Text("AHHAHAH")
+
 }
+
+
